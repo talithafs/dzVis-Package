d83886ad0014373daf66b615a032371da99acf57
talithafs <talitha.speranza@gmail.com>
2015-12-28 20:53:35 -0300

    Added util.R

--- util.R
+++ util.R
@@ -1,4 +1,4 @@
-updateFrontEnd <- function(){
+updateFrontEnd <- function(run = TRUE){
   wamp <- "C:\\Users\\Talitha\\Dropbox\\Economia\\Projeto Final\\R Package\\dzVis\\www\\"
   dzvis <- "C:\\Users\\Talitha\\Documents\\R\\win-library\\3.2\\dzVis\\"
   www <- paste(dzvis, "\\www", sep="")
@@ -8,4 +8,42 @@ updateFrontEnd <- function(){
   }
 
   file.copy(wamp,dzvis,recursive=TRUE)
+
+  if(run){
+    run()
+  }
 }
+
+rebuild <- function(run = TRUE, install = TRUE){
+  wamp <- "C:\\Users\\Talitha\\Dropbox\\Economia\\Projeto Final\\R Package\\dzVis\\www\\"
+  dzvis <- "C:\\Users\\Talitha\\Documents\\R\\win-library\\3.2\\dzVis\\"
+  www <- paste(dzvis, "\\www", sep="")
+
+  if(file.exists(www)) {
+    unlink(www, recursive = TRUE)
+  }
+
+  require(devtools)
+  devtools::build()
+
+  if(install){
+    devtools::install()
+  }
+
+  file.copy(wamp,dzvis,recursive=TRUE)
+
+  if(run){
+    run()
+  }
+
+}
+
+run <- function(){
+  require(opencpu)
+  if(!is.null(opencpu$url())){
+    opencpu$restart()
+  }
+  opencpu$browse("/library/dzVis/www")
+}
+
+
