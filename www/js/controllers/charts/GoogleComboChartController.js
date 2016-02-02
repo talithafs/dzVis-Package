bd7eeb497da99dc40ab8a5c21231dd942c4caea6
talithafs <talitha.speranza@gmail.com>
2016-02-02 11:57:50 -0300

    First part of GoogleChartsBase was refactored

--- www/js/controllers/charts/GoogleComboChartController.js
+++ www/js/controllers/charts/GoogleComboChartController.js
@@ -1,65 +1,45 @@
-application.controller("GoogleComboChartController", [ "$scope", "connection", "googlecharts", function($scope, connection, gCharts){
+application.controller("GoogleComboChartController", [ "$scope", "connection", "googlecharts", function($scope, connection, base){
 	
-	// // Google Charts labels
-	// $scope.targetVarLabel = gCharts.LBL_TARGET ;
-	// $scope.groupVarLabel = gCharts.LBL_GROUP ;
-	// $scope.timeVarLabel = gCharts.LBL_TIME ;
-	// $scope.filtersLabel = gCharts.LBL_FILTERS ;
-	// $scope.minLabel = gCharts.LBL_MIN_DATE ;
-	// $scope.maxLabel = gCharts.LBL_MAX_DATE ;
-	// $scope.placeholder = gCharts.PLH_MULTIPLE_DATES ;
-	// $scope.selectMultipleDatesLabel = gCharts.LBL_MULTIPLE_DATES ;
-// 	
-	// // Google Charts default values
-	// $scope.targetOptions = gCharts.DEFAULT_TARGET ;
-	// $scope.groupOptions = gCharts.DEFAULT_GROUP ;
-	// $scope.timeVar = gCharts.DEFAULT_TIME ;
-// 	
-	// $scope.targetSelection = gCharts.DEFAULT_TARGET[0] ;
-	// $scope.groupSelection = gCharts.DEFAULT_GROUP[0];
-// 	
-	// // New features labels
-	// $scope.lineVarLabel = "Desenhar linha com" ;
-	// $scope.operationLabel = "Operador" ;
-// 	
-	// // New features defaults
-	// var DEFAULT_LINE = [{id : null, text : "Não desenhar linha"}] ;
-// 	
-	// var DEFAULT_OPERATIONS = [
-		// {name: "Nenhuma", value: "none"},
-		// {name: "Desvio Padrão", value: "std"},
-		// {name: "Média", value: "mean"}
-	// ];
-// 	
-	// $scope.lineOptions = DEFAULT_LINE ;
-	// $scope.operations = DEFAULT_OPERATIONS ;
-	// $scope.lineSelection = DEFAULT_LINE[0];
-	// $scope.operationSelection = DEFAULT_OPERATIONS[0].value ;
+	// Google Charts labels
+	$scope.targetVarLabel =  base.LABEL.TARGET;
+	$scope.groupVarLabel = base.LABEL.GROUP ;
+	$scope.timeVarLabel = base.LABEL.TIME ;
+	$scope.filtersLabel = base.LABEL.FILTERS ;
+	$scope.minLabel = base.LABEL.MIN_DATE ;
+	$scope.maxLabel = base.LABEL.MAX_DATE ;
+	$scope.placeholder = base.PLACEHOLDER.MULTIPLE_DATES ;
+	$scope.selectMultipleDatesLabel = base.LABEL.MULTIPLE_DATES ;
+	
+	// Google Charts default values
+	$scope.targetOptions = base.DEFAULT.TARGET ;
+	$scope.groupOptions = base.DEFAULT.GROUP ;
+	$scope.timeVar = base.DEFAULT.TIME ;
+	
+	$scope.targetSelection = $scope.targetOptions[0] ;
+	$scope.groupSelection = $scope.groupOptions[0] ;
+	
+	// New features labels
+	$scope.lineVarLabel = "Desenhar linha com" ;
+	$scope.operationLabel = "Operador" ;
+	
+	// New features defaults	
+	var DEFAULT_LINE = [{id : null, text : "Não desenhar linha"}] ;
+	
+	var DEFAULT_OPERATIONS = [
+		{name: "Nenhuma", value: "none"},
+		{name: "Desvio Padrão", value: "std"},
+		{name: "Média", value: "mean"}
+	];
+	
+	$scope.lineOptions = DEFAULT_LINE ;
+	$scope.operations = DEFAULT_OPERATIONS ;
+	$scope.lineSelection = DEFAULT_LINE[0];
+	$scope.operationSelection = DEFAULT_OPERATIONS[0].value ;
 
 	
 	$scope.$on("treeClicked", function(e, instance){
 		
-		//gCharts.onTreeClicked(e, instance) ;
-		
-		// testes
-		// alert(gCharts.test("ola"));
-		// gCharts.setSomething = "trolha" ;
-// 		
-		// alert(gCharts.test("ooooi"));
-// 		
-		// alert(gCharts.other("siiiim"));
-		
-		alert(gCharts.constants.bla + " " + gCharts.constants.fu);
-		
-		gCharts.constants.bla = "fuck" ;
-		
-		alert(gCharts.constants.bla);
-		
-		gCharts.constants = "dirty" ;
-		
-		alert(gCharts.getConst().bla);
-		
-		
+		//base.onTreeClicked(e, instance) ;
 	});
 	
 	$scope.selectMultipleDates = function(){
@@ -67,10 +47,10 @@ application.controller("GoogleComboChartController", [ "$scope", "connection", "
 		$scope.multipleDatesMode = !$scope.multipleDatesMode ;
 		
 		if($scope.multipleDatesMode){
-			$scope.selectMultipleDatesLabel = gCharts.LBL_INTERVAL ;
+			$scope.selectMultipleDatesLabel = base.LABEL.INTERVAL ;
 		}
 		else {
-			$scope.selectMultipleDatesLabel = gCharts.LBL_MULTIPLE_DATES ;
+			$scope.selectMultipleDatesLabel = base.LABEL.MULTIPLE_DATES ;
 		}
 	};
 	
--- www/js/services/GoogleChartsBase.js
+++ www/js/services/GoogleChartsBase.js
@@ -1,30 +1,67 @@
 services.service("googlecharts", ["databasetree", "connection", function(databaseTree, connection){
-	return new GoogleChartsBase(databaseTree, connection);
+	return GoogleChartsBase.getInstance(databaseTree, connection);
 }]);
 
 
-//var GoogleChartsBase = (function(databaseTree, connection) {
+var GoogleChartsBase = (function() {
+	
+	var instance ;
+	
+	// Default Values
+	var DEFAULT = {
+		get TARGET() {
+			return [{id : null, text : "Nenhuma"}] ;
+		},
+		get GROUP() {
+			return [{id : null, text : "Não agrupar"}] ;
+		},
+		get TIME() {
+			return {id : null, text : "Nenhuma"} ;
+		}
+	};
+	
+	// Labels 
+	var LABEL = {
+		get MULTIPLE_DATES(){
+			return "Selecionar múltiplas datas" ;
+		},
+		get INTERVAL(){
+			return "Selecionar intervalo" ;
+		},
+		get TARGET(){
+			return "Variáveis numéricas" ; 
+		},
+		get GROUP(){
+			return "Agrupar por" ;
+		},
+		get TIME(){
+			return "Variável temporal";
+		},
+		get FILTERS(){
+			return "Filtros" ;
+		},
+		get MIN_DATE(){
+			return "Início:" ;
+		},
+		get MAX_DATE(){
+			return "Fim:" ;
+		} 
+	};
+
+	// Placeholders 
+	var PLACEHOLDER = {
+		get MULTIPLE_DATES(){
+			return "Inserir datas no formato dd-mm-aaaa, separadas por vírgulas" ;
+		}
+	};
+
+	function createInstance(databaseTree, connection) {
+        var object = new GoogleChartsBase(databaseTree, connection);
+        return object;
+    }
 	
 	function GoogleChartsBase(databaseTree, connection) {
 
-		// Default values 
-		var DEFAULT_TARGET = [{id : null, text : "Nenhuma"}] ;
-		var DEFAULT_GROUP = [{id : null, text : "Não agrupar"}];
-		var DEFAULT_TIME = {id : null, text : "Nenhuma"} ;
-		
-		// Labels
-		var LBL_MULTIPLE_DATES = "Selecionar múltiplas datas" ;
-		var LBL_INTERVAL = "Selecionar intervalo" ;
-		var LBL_TARGET = "Variáveis numéricas" ; 	
-		var LBL_GROUP = "Agrupar por" ;
-		var LBL_TIME = "Variável temporal";
-		var LBL_FILTERS = "Filtros" ;
-		var LBL_MIN_DATE = "Início:" ;
-		var LBL_MAX_DATE = "Fim:" ;
-		
-		// Placeholders
-		var PLH_MULTIPLE_DATES = "Inserir datas no formato dd-mm-aaaa, separadas por vírgulas" ;
-		
 		// Variables 
 		var currentTable = undefined ;
 		var validationMatrix = undefined ;
@@ -69,159 +106,24 @@ services.service("googlecharts", ["databasetree", "connection", function(databas
 			}	
 			
 		};
-		
-		
-		// Tests 
-		var something = null ;
-		 		
-		var setSomething = function(value){
-			something = value ;
-			return something ;
-		};
-		
-		GoogleChartsBase.prototype.test = function(value){
-			return setSomething.call(this, value);
-		};
-		
-		var other = null ;
-		
-		var setOther = function(value){
-			other = value ;
-			return other ;
-		};
-		
-		GoogleChartsBase.prototype.other = function(value){
-			return setOther.call(this, value);
-		};
-		
-		var constants = {
-			get bla() {
-				return "blabla";
-			},
-			get fu(){
-				return "fufu" ;
-			}
-		};
-		
-		GoogleChartsBase.prototype.constants = constants ;
-		
-		GoogleChartsBase.prototype.getConst = function(){
-			return constants ;
-		};
-		
-		//Public API
-		// return {
-			// DEFAULT_TARGET : DEFAULT_TARGET,
-			// DEFAULT_GROUP : DEFAULT_GROUP,
-			// DEFAULT_TIME : DEFAULT_TIME,
-			// LBL_MULTIPLE_DATES : LBL_MULTIPLE_DATES,
-			// LBL_INTERVAL : LBL_INTERVAL,
-			// LBL_TARGET : LBL_TARGET,
-			// LBL_GROUP : LBL_GROUP,
-			// LBL_TIME : LBL_TIME,
-			// LBL_FILTERS : LBL_FILTERS,
-			// LBL_MIN_DATE : LBL_MIN_DATE,
-			// LBL_MAX_DATE : LBL_MAX_DATE,
-			// PLH_MULTIPLE_DATES : PLH_MULTIPLE_DATES,
-			// onTreeClicked : onTreeClicked,
-		// };
 	}
 	
-	// function doSomething(value){
-		// return this.setSomething(value);
-	// }
+	GoogleChartsBase.prototype.DEFAULT = DEFAULT ;
+	GoogleChartsBase.prototype.LABEL = LABEL ;
+	GoogleChartsBase.prototype.PLACEHOLDER = PLACEHOLDER ;
+	
+	return {
+        getInstance: function (databaseTree, connection) {
+            if (!instance) {
+                instance = createInstance(databaseTree, connection);
+            }
+            return instance;
+        }
+    };
+}());
+
 
 	
-	//return GoogleChartsBase ;
-	
-//}());
-
-
 	
 	
-	
-
-// function GoogleChartsBase(databaseTree, connection) {
-// 	
-	// // Default values 
-	// var DEFAULT_TARGET = [{id : null, text : "Nenhuma"}] ;
-	// var DEFAULT_GROUP = [{id : null, text : "Não agrupar"}];
-	// var DEFAULT_TIME = {id : null, text : "Nenhuma"} ;
-// 	
-	// // Labels
-	// var LBL_MULTIPLE_DATES = "Selecionar múltiplas datas" ;
-	// var LBL_INTERVAL = "Selecionar intervalo" ;
-	// var LBL_TARGET = "Variáveis numéricas" ; 	
-	// var LBL_GROUP = "Agrupar por" ;
-	// var LBL_TIME = "Variável temporal";
-	// var LBL_FILTERS = "Filtros" ;
-	// var LBL_MIN_DATE = "Início:" ;
-	// var LBL_MAX_DATE = "Fim:" ;
-// 	
-	// // Placeholders
-	// var PLH_MULTIPLE_DATES = "Inserir datas no formato dd-mm-aaaa, separadas por vírgulas" ;
-// 	
-	// // Variables 
-	// var currentTable = undefined ;
-	// var validationMatrix = undefined ;
-	// var lastCheckedId = "" ;
-// 	
-	// // Functions
-	// var onTreeClicked = function(e, instance){
-// 		
-		// var checked = instance.get_checked(true);
-		// var dim = checked.length ; 
-// 		
-		// if(checked[dim-1] != undefined){
-// 			
-			// var justChecked = checked[dim-1] ;
-			// var table = databaseTree.getCurrentTable(justChecked) ;
-			// var column = "" ;
-			// var colNames = "" ;
-// 			
-			// if(table != currentTable){
-				// currentTable = table ;
-				// validationMatrix = [] ;
-				// lastCheckedId = "" ;
-			// }
-// 			
-			// if(justChecked.type == "lvl"){
-				// column = instance.get_parent(justChecked);
-			// }
-			// else {
-				// column = justChecked.id ;
-			// }
-// 			
-			// var colNames = validationMatrix.map(function(value,index) { 
-				// return value[0] ; 
-			// });
-// 			
-			// if(justChecked.id != lastCheckedId && colNames.indexOf(column) == -1 ){
-				// // validationMatrix.push([column, 'heey']);
-				// // alert(validationMatrix);
-			// }
-// 			
-			// lastCheckedId = justChecked.id ;
-		// }	
-// 		
-	// };
-// 	
-	// // Public API
-	// return {
-		// DEFAULT_TARGET : DEFAULT_TARGET,
-		// DEFAULT_GROUP : DEFAULT_GROUP,
-		// DEFAULT_TIME : DEFAULT_TIME,
-		// LBL_MULTIPLE_DATES : LBL_MULTIPLE_DATES,
-		// LBL_INTERVAL : LBL_INTERVAL,
-		// LBL_TARGET : LBL_TARGET,
-		// LBL_GROUP : LBL_GROUP,
-		// LBL_TIME : LBL_TIME,
-		// LBL_FILTERS : LBL_FILTERS,
-		// LBL_MIN_DATE : LBL_MIN_DATE,
-		// LBL_MAX_DATE : LBL_MAX_DATE,
-		// PLH_MULTIPLE_DATES : PLH_MULTIPLE_DATES,
-		// onTreeClicked : onTreeClicked
-	// };
-// }
-
 
